7f03e90153f2005fd59f765bb9fb1ca317e204a1
sahil <sahilagichani@gmail.com>
2024-02-23 20:20:55 +0530

    initial setup via spring initializer

---
+++ src/main/java/testswithspring/TestsWithSpringApplication.java
@@ -0,0 +1,13 @@
+package testswithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/testswithspring/TestsWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package testswithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
